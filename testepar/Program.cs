9f749d30071f912dc906baf1e4cf391c579c1e17
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2018-10-29 21:24:26 -0400

    teste de numero par ou impar

---
+++ testepar/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace testepar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Validar numero = new Validar();
+
+            Console.WriteLine("Informe um numero: ");
+            numero.verificar(Int32.Parse(Console.ReadLine()));
+
+            Console.WriteLine("O numero e: " + numero.resposta());
+        }
+    }
+}
---
+++ testepar/Validar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace testepar
+{
+    public class Validar
+    {
+           private string resultado;
+
+            public void verificar (int num)
+            {
+                if (num % 2 == 0)
+                {
+                    resultado = "Par";
+                }
+                else
+                {
+                    resultado = "Impar";
+                }
+            }
+
+            public string resposta()
+            {
+                return resultado;
+            }
+    }
+}
